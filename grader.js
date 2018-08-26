ef1862e2b1a52933027c48585f6f2a9a8ea65fe3
JmPotato <ghzpotato@gmail.com>
2018-08-26 12:19:01 +0800

    cookie support and other improvements

--- grader.js
+++ grader.js
@@ -1,18 +1,27 @@
 'use strict';
 
-var jwxt_id = ''; //学号
-var jwxt_password = ''; //教务系统密码
-
-var fs = require("fs");
+var fs = require('fs');
+var ejs = require('ejs')
 var path = require('path');
+var express = require('express');
 var request = require('request');
 var iconv = require('iconv-lite');
 var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
 
-var express = require('express');
-var app = express();
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
- 
+
+var app = express();
+app.use(express.static(__dirname + '/public'));
+app.use(cookieParser());
+app.set('views', __dirname + '/public');
+app.set("view engine", "html");
+app.engine('html', require('ejs').renderFile);
+
+var server_url = 'http://127.0.0.1:8080';
+var jwxt_id = ''; //学号
+var jwxt_password = ''; //教务系统密码
+
 var post_headers = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/11.1.2 Safari/605.1.15',
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
@@ -31,15 +40,47 @@ var get_headers = {
     'Cookie': '',
 };
 
-app.use(express.static(__dirname + '/public'));
+app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", req.headers.origin);
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+    res.header("Access-Control-Allow-Credentials",true);
+    next();
+});
 
 app.get('/', function (req, res) {
-    function renderPage () {res.sendFile(__dirname + '/public/validate.html');};
+    var login = 0;
+    function renderPage () {res.render('validate', {login, server_url});};
+    if (req.cookies.user) {
+        login = 1;
+        jwxt_id = req.cookies.user.id;
+        jwxt_password = req.cookies.user.password;
+        request.get({url: 'http://jwxt.bupt.edu.cn/validateCodeAction.do?random=', encoding: null}, function (error, response, body) {
+            post_headers.Cookie = response.headers["set-cookie"].toString().substring(0, 32);
+            get_headers.Cookie = response.headers["set-cookie"].toString().substring(0, 32);
+        }).pipe(fs.createWriteStream(__dirname + '/public/validate_code.jpg'));
+    } else {
+        login = 0;
+        jwxt_id = '';
+        jwxt_password = '';
+    }
     setTimeout(renderPage, 3000);
-    request.get({url: 'http://jwxt.bupt.edu.cn/validateCodeAction.do?random=', encoding: null}, function (error, response, body) {
-        post_headers.Cookie = response.headers["set-cookie"].toString().substring(0, 32);
-        get_headers.Cookie = response.headers["set-cookie"].toString().substring(0, 32);
-    }).pipe(fs.createWriteStream(__dirname + '/public/validate_code.jpg'));
+});
+
+app.post('/sign_in', urlencodedParser, function (req, res) {
+    res.clearCookie('user');
+    var re_id = /^\d{10}$/;
+    if (re_id.test(req.body.id) && req.body.password !== '') {
+        res.cookie('user', {id: req.body.id,password: req.body.password},{maxAge:2678400000,path:'/',httpOnly:true});
+    }
+    res.redirect(server_url);
+});
+
+app.get('/sign_out', function (req, res) {
+    res.clearCookie('user');
+    jwxt_id = '';
+    jwxt_password = '';
+    res.redirect(server_url);
 });
 
 app.post('/get_grades', urlencodedParser, function (req, res) {
