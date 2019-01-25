f7134e74827fd34672c87ad893d8b093120680b9
JmPotato <ghzpotato@gmail.com>
2019-01-25 23:31:13 +0800

    Small changes

--- config.js
+++ config.js
@@ -4,6 +4,7 @@ var config = {
     key: 'PLEASE CHANGE THIS!!!',
     net_user: '', //校园网帐号，用于内网验证
     net_password: '', //校园网密码
+    type: 0, //0 表示通过网页 VPN 访问教务系统，1 表示直接访问
 }
 
 module.exports = config;
